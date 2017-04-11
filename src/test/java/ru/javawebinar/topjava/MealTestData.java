27e684f96f1258b943c666653b9c52f7e6a385f1
Drag <drag.faq@gmail.com>
2017-04-11 23:17:42 +0300

    HW 3.5

--- src/test/java/ru/javawebinar/topjava/MealTestData.java
+++ src/test/java/ru/javawebinar/topjava/MealTestData.java
@@ -16,13 +16,6 @@ public class MealTestData {
     public static final Meal MEAL1 = new Meal(MEAL1_ID, LocalDateTime.of(2017, Month.APRIL, 13, 10, 10), "aaa", 250);
     public static final Meal MEAL2 = new Meal(MEAL2_ID, LocalDateTime.of(2017, Month.APRIL, 13, 12, 10), "bbb", 350);
 
-    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>(
-            (expected, actual) -> expected == actual ||
-            (Objects.equals(expected.getCalories(), actual.getCalories())
-            && Objects.equals(expected.getId(), actual.getId())
-            && Objects.equals(expected.getDateTime(), actual.getDateTime())
-            && Objects.equals(expected.getDescription(), actual.getDescription())
-            )
-            );
+    public static final ModelMatcher<Meal> MATCHER = new ModelMatcher<>();
 
 }
