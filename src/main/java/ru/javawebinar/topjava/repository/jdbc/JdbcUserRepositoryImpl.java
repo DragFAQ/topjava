9a0c9850553d5a733ff2f4db4956c769316cbc0b
Drag <drag.faq@gmail.com>
2017-05-10 00:19:47 +0300

    HW 6.2.4.1

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcUserRepositoryImpl.java
@@ -1,24 +1,31 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.dao.support.DataAccessUtils;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.ResultSetExtractor;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+import ru.javawebinar.topjava.model.Role;
 import ru.javawebinar.topjava.model.User;
 import ru.javawebinar.topjava.repository.UserRepository;
 
 import javax.sql.DataSource;
-import java.util.List;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.*;
 
 @Repository
 public class JdbcUserRepositoryImpl implements UserRepository {
 
     private static final BeanPropertyRowMapper<User> ROW_MAPPER = BeanPropertyRowMapper.newInstance(User.class);
+
+    private static final BeanPropertyRowMapper<String> ROLE_MAPPER = BeanPropertyRowMapper.newInstance(String.class);
 
     private final JdbcTemplate jdbcTemplate;
 
@@ -58,21 +65,57 @@ public class JdbcUserRepositoryImpl implements UserRepository {
         return jdbcTemplate.update("DELETE FROM users WHERE id=?", id) != 0;
     }
 
+    private Set<Role> getRoles(int id) {
+        List<String> strRoles = jdbcTemplate.queryForList("SELECT role FROM user_roles WHERE user_id=" + id, String.class);
+        Set<Role> roles = new HashSet<>();
+        for (String s : strRoles)
+            roles.add(Role.valueOf(s));
+
+        return roles;
+    }
+
     @Override
     public User get(int id) {
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE id=?", ROW_MAPPER, id);
-        return DataAccessUtils.singleResult(users);
+        User user = DataAccessUtils.singleResult(users);
+        if (user != null)
+            user.setRoles(getRoles(id));
+        return user;
     }
 
     @Override
     public User getByEmail(String email) {
 //        return jdbcTemplate.queryForObject("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
         List<User> users = jdbcTemplate.query("SELECT * FROM users WHERE email=?", ROW_MAPPER, email);
-        return DataAccessUtils.singleResult(users);
+        User user = DataAccessUtils.singleResult(users);
+        user.setRoles(getRoles(user.getId()));
+        return user;
     }
 
     @Override
     public List<User> getAll() {
-        return jdbcTemplate.query("SELECT * FROM users ORDER BY name, email", ROW_MAPPER);
+        return (List<User>) jdbcTemplate.query("SELECT * FROM users JOIN user_roles ON user_id = id ORDER BY name, email", (new UserExtractor()));
+    }
+
+    private class UserExtractor implements ResultSetExtractor {
+
+        @Override
+        public Object extractData(ResultSet rs) throws SQLException, DataAccessException {
+            Map<Integer, User> map = new HashMap<>();
+            User user = null;
+            while (rs.next()) {
+                Integer id = rs.getInt("id");
+                user = map.get(id);
+                if (user == null) {
+                    user = new User(id, rs.getString("name"), rs.getString("email"),
+                            rs.getString("password"), rs.getInt("calories_per_day"),
+                            rs.getBoolean("enabled"), new HashSet<>());
+                    map.put(id, user);
+                }
+                user.getRoles().add(Role.valueOf(rs.getString("role")));
+            }
+
+            return new ArrayList<User>(map.values());
+        }
     }
 }
--- src/test/java/ru/javawebinar/topjava/UserTestData.java
+++ src/test/java/ru/javawebinar/topjava/UserTestData.java
@@ -15,6 +15,10 @@ public class UserTestData {
     public static final User USER = new User(USER_ID, "User", "user@yandex.ru", "password", Role.ROLE_USER);
     public static final User ADMIN = new User(ADMIN_ID, "Admin", "admin@gmail.com", "admin", Role.ROLE_ADMIN);
 
+    static {
+        ADMIN.getRoles().add(Role.ROLE_USER);
+    }
+
     public static final ModelMatcher<User> MATCHER = new ModelMatcher<>(
             (expected, actual) -> expected == actual ||
                     (Objects.equals(expected.getPassword(), actual.getPassword())
